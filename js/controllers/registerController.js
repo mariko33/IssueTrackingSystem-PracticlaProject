b32ef11b2fe44d6cd6e08bce642daa5c90a4c962
mariko33 <maria.t.georgieva2012@gmail.com>
2016-04-20 12:10:42 +0300

    addNew

--- js/controllers/registerController.js
+++ js/controllers/registerController.js
@@ -5,16 +5,6 @@ app.controller('RegisterController',
     function ($scope, $location, authService, notifyService) {
         $scope.userData = {};
 
-       /* $scope.register = function (user) {
-                            authService.registerUser(user)
-                                .then(function(userData) {
-                                        console.log(userData);
-                                    });
-                       };
-        console.log(userData);
-*/
-
-
         $scope.register = function(userData) {
             authService.register(userData,
                 function success() {
@@ -26,7 +16,6 @@ app.controller('RegisterController',
                     notifyService.showError("Failed to register", err);
                     $location.path('/');
                 })
-            console.log(userData);
         }
     }
 );
